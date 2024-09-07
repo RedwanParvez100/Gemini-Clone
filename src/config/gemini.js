238c2dcb7dcf2d68e6ce4cbc3259fd0e64bd52eb
RedwanParvez100 <19202103100@cse.bubt.edu.bd>
2024-09-07 18:20:45 +0600

    updated

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -7,6 +7,7 @@
  */
 
 
+
 import {
   GoogleGenerativeAI,
   HarmCategory,
@@ -72,21 +73,78 @@ export default run;
 
 
 
+/*
+ * Install the Generative AI SDK
+ *
+ * $ npm install @google/generative-ai
+ */
+
+/* yt
+
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai"
+
+const MODEL_NAME = "gemini-1.0-pro";
+const API_KEY = "AIzaSyCgbdDzk5iBJDMK-dcNvyM7k9wsAihZctc";
+
+async function run(prompt) {
+  const genAI = new GoogleGenerativeAI(API_KEY);
+  const model = genAI.getGenerativeModel({model:MODEL_NAME});
+
+  const generationConfig = {
+    temperature: 0.9,
+    topP: 1,
+    topK: 1,
+    maxOutputTokens: 2048,
+   // responseMimeType: "text/plain",
+  };
+  
+
+  const safetySettings = [
+
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+
+    },
+  ];
+
+  const chat = model.startChat({
+    generationConfig,
+    safetySettings,
+    history:[
+
+    ],
+
+  });
 
 
+  const result = await chat.sendMessage(prompt);
+  const response = result.response;
+  console.log(response.text());
+}
 
+export default run;
 
-
-
-
-
-
-
-
-
-
-
-
+/*
 
 
 
